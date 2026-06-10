763179b34ca43b1dfe6842ee498829c45de07114
JayRajAada <jayrajaada@gmail.com>
2026-06-10 14:53:50 +0530

    Done on 10-06-2026

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void findCombination(int ind , vector<int>& arr , vector<vector<int>>& ans , vector<int>& ds , int target){
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i = ind ; i < arr.size() ; i++){
+            if(i > ind && arr[i] == arr[i - 1]) continue;
+            if(arr[i] > target) break;
+            ds.push_back(arr[i]);
+            findCombination(i + 1 , arr , ans , ds , target - arr[i]);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin() , candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0 , candidates , ans , ds , target);
+        return ans;
+    }
+};
