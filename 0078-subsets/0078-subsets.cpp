74d789e1b3908be8af46957898adc28c362fe898
JayRajAada <jayrajaada@gmail.com>
2026-07-12 18:56:40 +0530

    Done on 12-07-2026

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        int no_of_subsets = 1 << nums.size();
+        for (int num = 0; num < no_of_subsets; num++)
+        {
+            vector<int> list;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (num & (1 << i))
+                {
+                    list.push_back(nums[i]);
+                }
+            }
+            ans.push_back(list);
+        }
+        return ans;
+    }
+};
