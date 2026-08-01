7b4fc26907839ce423318af4c2590669d3006e4a
JayRajAada <jayrajaada@gmail.com>
2026-08-01 21:59:16 +0530

    Done on 01-08-2026

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int max_area = 0;
+        int n = heights.size();
+        for (int i = 0; i <= n; i++) {
+
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if (st.empty()) {
+                    width = i;
+                } else {
+                    width = i - st.top() - 1;
+                }
+                max_area = max(max_area, width * height);
+            }
+            st.push(i);
+        }
+        return max_area;
+    }
+};
