afdd8e1cd980a0b3ca84fa45281b95838ed8f313
JayRajAada <jayrajaada@gmail.com>
2026-08-20 22:05:00 +0530

    Done on 20-08-2026

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        bool isJewel[128] = {false};
+        for (char j : jewels) {
+            isJewel[j] = true;
+        }
+        int jewelCount = 0;
+        for (char s : stones) {
+            if (isJewel[s]) {
+                jewelCount++;
+            }
+        }
+        return jewelCount;
+    }
+};
