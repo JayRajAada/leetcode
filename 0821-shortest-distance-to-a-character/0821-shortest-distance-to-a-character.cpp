ad0f9d0d80ba0cdd1194bfdbf0d865009eb6916d
JayRajAada <jayrajaada@gmail.com>
2026-08-28 16:14:47 +0530

    Done on 28-08-2026

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int n = s.length();
+        vector<int> ans(n, n);
+        int pos = -n;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == c)
+                pos = i;
+            ans[i] = i - pos;
+        }
+        for (int i = pos - 1; i >= 0; --i) {
+            if (s[i] == c)
+                pos = i;
+            ans[i] = min(ans[i], pos - i);
+        }
+        return ans;
+    }
+};
